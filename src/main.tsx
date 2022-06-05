475aa261950c7322077e35792ab2d0196d543808
Stefano Mitello <stefano@mono.studio>
2022-06-06 01:20:00 +0200

    removed strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import App from './components/App'
+import './global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //se strict mode è attivata useeffct run twice
+  <App />
 )

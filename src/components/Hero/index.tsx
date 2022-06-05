3f7b2dfe58c8861dd542d7d40f52c9f22a835c2b
Stefano Mitello <stefano@mono.studio>
2022-06-06 01:23:08 +0200

    feat: create hero component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './style.scss'
+
+
+
+function Hero() {
+
+    return (
+        <>
+            <h1 className='mb-10 font-bold bg-clip-text text-transparent bg-gradient-to-b from-glow-10 to-glow-20 text-shadow'>Title</h1>
+            
+            <p className='text-lg font-medium text-text-10'>Subtitle</p>
+        </>
+    );
+}
+
+export default Hero

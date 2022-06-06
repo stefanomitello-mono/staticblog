71becaab87fe74f3e30b9b9fc72fab4ac18ebd4b
Stefano Mitello <stefano@mono.studio>
2022-06-06 17:22:22 +0200

    fix link

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import svgIcon from '../../assets/icons.svg';
+import { Link } from "react-router-dom";
+
+
 
 type Props = {
     title: string,
@@ -15,10 +18,10 @@ const Button = ({ title, uri, isButton = true, icon, backgroundColor, textColor 
 
     return (
         <>
-            <a className={`px-3 py-1 flex items-center text-base capitalize font-sans text-${textColor} hover:opacity-75 ${backgroundColor ? (`bg-${backgroundColor}`) : ('')} ${isButton ? ('border-2 border-solid border-[#7cc9ff] border-opacity-40 rounded-md hover:bg-blue-30 hover:text-glow-10') : ('')}`} href={uri}>
+            <Link className={`px-3 flex items-center justify-center text-center mx-text-base capitalize font-sans text-${textColor} hover:opacity-75 ${backgroundColor ? (`bg-${backgroundColor}`) : ('')} ${isButton ? ('border-2 border-solid border-[#7cc9ff] border-opacity-40 rounded-md hover:bg-blue-30 hover:text-glow-10') : ('')}`} to={uri}>
                 {isButton ?
                     (
-                        <button>
+                        <button className='py-1'>
                             {icon && (
                                 <svg className="mr-4 w-4" viewBox="0 0 25 20" xmlns="http://www.w3.org/2000/svg">
                                     <use href={`${svgIcon}#${icon}`} />
@@ -26,11 +29,10 @@ const Button = ({ title, uri, isButton = true, icon, backgroundColor, textColor 
                             )}
                             {title}
                         </button>
-                    ) : (
-                        title
-                    )
+                    ) :
+                    (title)
                 }
-            </a>
+            </Link>
         </>
     );
 
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 
 import axios from 'axios';
+import { Link } from "react-router-dom";
+
 
 import { Data } from "../../models/data";
 import svgIcon from '../../assets/icons.svg';
@@ -25,16 +27,16 @@ function Navbar() {
         <>
             <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 max-w-screen-md mx-auto">
                 <div className="container mx-auto flex flex-wrap items-center justify-between">
-                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        <a
+                    <div className="w-full relative flex justify-between items-baseline lg:w-auto lg:static lg:block lg:justify-start">
+                        <Link
                             className="text-lg font-bold inline-flex py-2 whitespace-nowrap capitalize text-text-10"
-                            href={data?.site.baseUri}
+                            to={data?.site.baseUri!}
                         >
                             <svg className="mr-4 w-4" viewBox="0 0 25 20" xmlns="http://www.w3.org/2000/svg">
                                 <use href={`${svgIcon}#${data?.site.icon}`} />
                             </svg>
                             {data?.site.name}
-                        </a>
+                        </Link>
                         <button
                             className="text-white cursor-pointer px-3 py-1 block lg:hidden"
                             type="button"
@@ -55,9 +57,9 @@ function Navbar() {
                         }
                         id="example-navbar-danger"
                     >
-                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                        <ul className="flex flex-col items-center lg:flex-row list-none lg:ml-auto">
                             {data?.navbar.map(item => (
-                                <li key={item.name} className="nav-item mx-4">
+                                <li key={item.name} className="nav-item mx-4 ">
                                     <Button title={item.name} uri={item.uri} isButton={item.isButton} textColor={'white'} />
                                 </li>
                             ))}

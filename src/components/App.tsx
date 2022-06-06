06d4c1f79437d7698bd5f4d7f67255c66e650e7c
Stefano Mitello <stefano@mono.studio>
2022-06-06 17:21:52 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,7 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react'
 
-import svgIcon from '../assets/icons.svg';
-import { Articles } from '../models/articles';
 
 
-import Button from './Button'
-import Hero from './Hero'
-import Navbar from './Navbar'
-import Project from './ProjectCard';
 
 import {
   BrowserRouter as Router,
@@ -23,15 +16,7 @@ import Feed from '../page/articles';
 
 function App() {
 
-  const [articles, setArticles] = useState<Articles>();
 
-  useEffect(() => {
-    axios.get<Articles>('/src/public/articles.json')
-      .then((response) => {
-        console.log(response.data);
-        setArticles(response.data);
-      });
-  }, []);
 
   return (
     <Router>

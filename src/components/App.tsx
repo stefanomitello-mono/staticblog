0845acf247d4a7608255829c03ebb3c0c9073e72
Stefano Mitello <stefano@mono.studio>
2022-06-06 17:21:18 +0200

    added router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,47 @@
-import { useState } from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+
+import svgIcon from '../assets/icons.svg';
+import { Articles } from '../models/articles';
+
+
+import Button from './Button'
 import Hero from './Hero'
 import Navbar from './Navbar'
+import Project from './ProjectCard';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from '../page/homepage';
+import Feed from '../page/articles';
 
 
 
 function App() {
-  return (
-    <>
-      <Navbar></Navbar>
 
-      <div className="mt-28 max-w-screen-md mx-auto px-2 ">
-        <Hero />
-      </div>
-    </>
+  const [articles, setArticles] = useState<Articles>();
+
+  useEffect(() => {
+    axios.get<Articles>('/src/public/articles.json')
+      .then((response) => {
+        console.log(response.data);
+        setArticles(response.data);
+      });
+  }, []);
+
+  return (
+    <Router>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/articles">
+        <Feed />
+      </Route>
+    </Router>
   )
 }
 

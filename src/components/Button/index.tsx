2607e339df2580fb362cac4e70a229b99092fe77
Stefano Mitello <stefano@mono.studio>
2022-06-06 10:10:25 +0200

    feat: added props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
-
+import svgIcon from '../../assets/icons.svg';
 
 type Props = {
     title: string,
     uri: string,
-    isButton: boolean
+    isButton: boolean,
+    icon?: string,
+    backgroundColor?: string,
+    textColor: string
 };
 
-const Button = ({ title, uri, isButton = true }: Props) => {
+const Button = ({ title, uri, isButton = true, icon, backgroundColor, textColor }: Props) => {
 
 
     return (
         <>
-            <a className={`px-3 py-1 flex items-center text-base capitalize font-sans text-text-10 hover:opacity-75 ${isButton ? 'border-2 border-solid border-[#7cc9ff] border-opacity-40 rounded-md' : ''}`} href={uri}>
+            <a className={`px-3 py-1 flex items-center text-base capitalize font-sans text-${textColor} hover:opacity-75 ${backgroundColor ? (`bg-${backgroundColor}`) : ('')} ${isButton ? ('border-2 border-solid border-[#7cc9ff] border-opacity-40 rounded-md hover:bg-blue-30 hover:text-glow-10') : ('')}`} href={uri}>
                 {isButton ?
                     (
-                        <button className=''>{title}</button>
+                        <button>
+                            {icon && (
+                                <svg className="mr-4 w-4" viewBox="0 0 25 20" xmlns="http://www.w3.org/2000/svg">
+                                    <use href={`${svgIcon}#${icon}`} />
+                                </svg>
+                            )}
+                            {title}
+                        </button>
                     ) : (
                         title
                     )

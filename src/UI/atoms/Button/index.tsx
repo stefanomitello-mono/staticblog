18c64b085c1751b6af993852a1e1b2a8502776a7
Stefano Mitello <stefano@mono.studio>
2022-06-08 22:35:09 +0200

    Create button component

---
+++ src/UI/atoms/Button/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Link } from "react-router-dom";
+
+type Props = {
+    title: string,
+    uri: string,
+    icon?: string,
+    backgroundColor?: string,
+    textColor?: string
+};
+
+const Button = ({ title, uri, icon, backgroundColor = 'transparent', textColor = 'white' }: Props) => {
+
+    console.log(title, uri, icon, backgroundColor, textColor)
+    return (
+        <>
+            <Link to={uri}>
+
+                <button type="button" className={`text-${textColor} bg-${backgroundColor} hover:text-glow-10 hover:bg-blue-30  capitalize font-sans rounded-lg border-2 border-solid border-[#7cc9ff] border-opacity-40 rounded-mdtext-sm px-5 py-2.5 text-center inline-flex items-center mr-2 `}>
+                    {icon && (
+                        <img src={`src/assets/icons/${icon}.png`} className="w-5 h-5 mr-2 -ml-1" />
+                    )}
+                    {title}
+                </button>
+            </Link>
+        </>
+    );
+
+}
+
+export default Button

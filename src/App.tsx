41f918f930c3416bc4f0c84f5e2e970bc7eac00b
Stefano Mitello <stefano@mono.studio>
2022-06-09 16:44:01 +0200

    layout page

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,56 @@ import { useEffect, useState } from 'react'
 import {
     BrowserRouter as Router,
     Route,
-    Link
+    Link,
+    Switch
 } from "react-router-dom";
 
+import Articles from './UI/pages/Articles';
 import Home from './UI/pages/Homepage';
-import Feed from './UI/pages/articles';
+import Header from './UI/organisms/Header';
+import { Data } from './models/data';
+import axios from 'axios';
+import About from './UI/pages/About';
+import Footer from './UI/organisms/Footer';
 
 
 
 function App() {
 
+
+    const [data, setData] = useState<Data>();
+
+    useEffect(() => {
+        axios.get<Data>('/src/public/data.json')
+            .then((response) => {
+                console.log("res:", response.data);
+                setData(response.data);
+            });
+    }, []);
+
     return (
-        <Router>
-            <Route path="/">
-                <Home />
-            </Route>
-            <Route path="/articles">
-                <Feed />
-            </Route>
-        </Router>
+        <>
+            <Router>
+                <Header titleSite={data?.site.name!} urlSite={data?.site.baseUri!} iconLogo={data?.site.icon} navItem={data?.navitems} />
+
+
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/articles">
+                        <Articles />
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                </Switch>
+
+                <Footer copyright={data?.site.copyright!} socials={data?.social!} />
+
+            </Router>
+
+        </>
     )
 }
 

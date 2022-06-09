8e45e09ab0d794796079f1ae147819030b95f7db
Stefano Mitello <stefano@mono.studio>
2022-06-09 09:53:49 +0200

    create h2 component

---
+++ src/UI/atoms/Header02/index.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+type Props = {
+    text: string,
+    textColor?: string,
+    className?: string
+};
+
+const Header02 = ({ text, textColor = 'text-10', className = "" }: Props) => {
+    return (
+        <>
+            <h2 className={`font-sans text-h2 md:text-md-h2 font-bold text-${textColor} ${className && (className)} px-5 py-1`}>
+                {text}
+            </h2>
+        </>
+    );
+}
+
+export default Header02  

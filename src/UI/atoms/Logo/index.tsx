ddb91a571f2702e78b10cdce98f2e1545e69f7b7
Stefano Mitello <stefano@mono.studio>
2022-06-09 16:42:17 +0200

    create logo atom

---
+++ src/UI/atoms/Logo/index.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Link } from "react-router-dom";
+
+type Props = {
+    logoImg: string,
+    title: string,
+    uri: string,
+    className?: string
+};
+
+const Logo = ({ logoImg, title, uri, className = "" }: Props) => {
+    return (
+        <>
+            <Link to={uri} className={`${className && (className)}`}>
+                {logoImg && (<img src={`./src/assets/${logoImg}.png`} className="mr-4 h-6 w-auto" />)}
+                {title}
+            </Link>
+        </>
+    );
+}
+
+export default Logo  

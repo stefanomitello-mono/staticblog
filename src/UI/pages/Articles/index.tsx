972471886237b7eb30681d9eef6d40f114a2312e
Stefano Mitello <stefano@mono.studio>
2022-06-13 10:13:10 +0200

    created articles page

---
+++ src/UI/pages/Articles/index.tsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import { Feed } from '../../../models/articles';
+import Header01 from '../../atoms/Header01';
+import Post from '../../molecules/Article';
+import SnippetArticle from '../../molecules/Article/previewArticle';
+import LatestArticles from '../../organisms/LatestArticles';
+
+function Articles() {
+
+
+    const [post, setPost] = useState<Feed>();
+
+
+    useEffect(() => {
+
+        axios.get<Feed>('/src/public/articles.json')
+            .then((response) => {
+                setPost(response.data);
+            });
+    }, []);
+
+
+    return (
+        <>
+
+
+            <div className='container px-2 py-3 my-24 max-w-screen-md mx-auto'>
+
+                <Header01 text={'Articles'} className='mb-14' />
+
+                {
+                    post && post.articles.map(item => (
+                        <Post key={item.uri} title={item.title} url={item.uri} category={item.category} publishedOn={item.publishedOn} body={item.body} className='mb-24' />
+                    ))
+                }
+            </div>
+
+
+        </>
+    )
+
+}
+
+export default Articles

79b2840fb026c1bd8c52090fe28faee2ad3626b7
Stefano Mitello <stefano@mono.studio>
2022-06-13 10:13:23 +0200

    created homepage page

---
+++ src/UI/pages/Homepage/index.tsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import { Feed } from '../../../models/articles';
+import { Data } from '../../../models/data';
+import Hero from '../../organisms/Hero';
+import LatestArticles from '../../organisms/LatestArticles';
+import Projects from '../../organisms/Projects';
+
+function Home() {
+
+
+    const [data, setData] = useState<Data>();
+    const [post, setPost] = useState<Feed>();
+
+
+    useEffect(() => {
+        axios.get<Data>('/src/public/data.json')
+            .then((response) => {
+                setData(response.data);
+            });
+
+        axios.get<Feed>('/src/public/articles.json')
+            .then((response) => {
+                setPost(response.data);
+            });
+    }, []);
+
+
+    return (
+        <>
+            <Hero title={data?.hero.title!} subtitle={data?.hero.subtitle!} className="mt-32" />
+
+            <LatestArticles title={'All articles'} allArticlesUri={'/articles'} feed={post!} />
+
+            <Projects project={data?.project!} />
+        </>
+    )
+}
+
+export default Home

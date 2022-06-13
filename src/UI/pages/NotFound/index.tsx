30fdabf5660056dcfb9487d024ce4771fd70b2ce
Stefano Mitello <stefano@mono.studio>
2022-06-13 10:13:34 +0200

    404 page created

---
+++ src/UI/pages/NotFound/index.tsx
@@ -0,0 +1,14 @@
+import Header01 from "../../atoms/Header01";
+
+
+function NotFound() {
+
+    return (
+        <div className="container mx-auto px-2 py-3 max-w-screen-md my-32">
+            <Header01 text={"Pagina non trovata :("} />
+        </div>
+
+    )
+}
+
+export default NotFound
